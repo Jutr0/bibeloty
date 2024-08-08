a401f0585cc795979ad66c9604fc99f5a4adacb9
Victor Kielczewski <victor.kielczewski@gdsoft.info>
2024-08-08 20:40:35 +0200

    project init

---
+++ src/containers/categories/Categories.js
@@ -0,0 +1,8 @@
+import "./Categories.scss"
+import Box from "../../common/box/Box";
+
+const Categories = () => {
+    return <Box className="categories">Categories</Box>;
+}
+
+export default Categories
