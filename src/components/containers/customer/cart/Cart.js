fc5021f5044707e4aa8a9b1235fec97b1e9c8418
wiktor <micwikski@gmail.com>
2025-01-12 20:59:40 +0100

    added checkout button

--- src/components/containers/customer/cart/Cart.js
+++ src/components/containers/customer/cart/Cart.js
@@ -3,15 +3,21 @@ import './Cart.scss';
 import {useSelector} from "react-redux";
 import Button from "../../../common/button/Button";
 import CartProduct from "./CartProduct";
+import {useNavigate} from "react-router";
 
 const Cart = () => {
 
     const products = useSelector(state => state.cart.products)
     const isEmpty = !products || products.length === 0;
+    const navigate = useNavigate();
+    const navigateToCheckout = () => {
+        navigate('/checkout')
+    }
 
     return <Container className="cart">
         <h2 className="header">{isEmpty ? 'KOSZYK JEST PUSTY' : 'PRODUKTY W KOSZYKU'}</h2>
         {!isEmpty && <>
+            <Button className="result" onClick={navigateToCheckout}>DO PODSUMOWANIA</Button>
             <div className="products">
                 {products.map(p => <CartProduct key={p.id} product={p}/>
                 )}
